be88e24ef057f28bb8fb765bc56fe8309ceb957f
gyumin-k <61161614+gyumin-k@users.noreply.github.com>
2025-01-13 15:15:04 +0900

    Update streamlit_refer.py

--- streamlit_refer.py
+++ streamlit_refer.py
@@ -3,10 +3,7 @@ import tiktoken
 from loguru import logger
 
 from langchain.chains import ConversationalRetrievalChain
-try:
-    from langchain.chat_models import ChatOpenAI
-except ImportError:
-    from langchain_community.chat_models import ChatOpenAI
+from langchain.chat_models import ChatOpenAI
 
 from langchain.document_loaders import PyPDFLoader
 from langchain.document_loaders import Docx2txtLoader
